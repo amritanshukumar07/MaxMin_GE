d1360f6038ad9e766b98030328cae286563f9466
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-26 00:27:42 +0530

    Maximum Integer found

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static Integer findMax(Integer a,Integer b,Integer c){
+        Integer max= a;
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+        return max;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to MIN-MAX Program");
+        System.out.println("Max in 1st :" + findMax(9,6,3));
+        System.out.println("Max in 2nd :" + findMax(6,9,3));
+        System.out.println("Max in 3rd :" + findMax(3,6,9));
     }
 }
